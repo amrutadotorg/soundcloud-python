e399914871d72b1981c4e8aca0a6f75989c3ba3c
amrutadotorg <amruta.org@gmail.com>
2026-01-22 23:18:35 +0100

    fix to 2.0 ver.

--- soundcloud/__init__.py
+++ soundcloud/__init__.py
@@ -1,8 +1,8 @@
 """Python Soundcloud API Wrapper."""
 
-__version__ = '0.6.0'
-__all__ = ['Client']
+__version__ = "2.0.0"
+__all__ = ["Client"]
 
-USER_AGENT = 'SoundCloud Python API Wrapper %s' % __version__
+USER_AGENT = "SoundCloud Python API Wrapper %s" % __version__
 
 from soundcloud.client import Client
